54436a0762162782bb18086d69269c5ba0692486
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-16 08:28:16 +0300

    ciphering data and creating jwt token

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
 	res.send('Hello world!')
+})
+
+app.post('/auth/login', (req, res) => {
+	const token = jwt.sign(
+		{
+			email: req.body.email,
+			fullName: 'Дима Пупкин',
+		},
+		'secret123'
+	)
+
+	res.json({
+		success: true,
+		token,
+	})
 })
 
 const port = 4444
