0d3b036a2704cd477ae913363cfa3088fb204997
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-18 08:39:16 +0300

    post updating method updated

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -105,19 +105,33 @@ export const remove = (req, res) => {
 export const update = async (req, res) => {
 	try {
 		const postId = req.params.id
-		await PostModel.updateOne({ _id: postId }, (err, doc) => {
-			if (err) {
-				return res.status(500).json({
-					message: 'Post deleting error',
-				})
+		await PostModel.updateOne(
+			{
+				_id: postId,
+			},
+			{
+				title: req.body.title,
+				text: req.body.text,
+				tags: req.body.tags,
+				user: req.userId,
+				imageUrl: req.body.imageUrl,
+			},
+			(err, doc) => {
+				if (err) {
+					console.log(err)
+					return res.status(500).json({
+						message: 'Post deleting error',
+					})
+				}
+				if (!doc) {
+					console.log(err)
+					return res.status(404).json({
+						message: 'Post not found',
+					})
+				}
+				res.json({ success: true })
 			}
-			if (!doc) {
-				return res.status(404).json({
-					message: 'Post not found',
-				})
-			}
-			res.json({ success: true })
-		})
+		)
 	} catch (error) {
 		res.status(500).json({
 			message: 'Post updating error',
