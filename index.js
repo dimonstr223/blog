d63b52f80394b255025bdd3bb2325f3ab7823340
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-18 06:25:37 +0300

    validations folder removed, validations.js file created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-import { registerValidation } from './validations/auth.js'
+import { registerValidation } from './validations.js'
 import checkAuth from './utils/checkAuth.js'
 import * as UserController from './controllers/UserController.js'
 
