b414d78dc398b72d977d8299be56215d1efc617c
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-18 06:21:23 +0300

    auth/me added, index.js refactored

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express'
-import { validationResult } from 'express-validator'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
 
 import { registerValidation } from './validations/auth.js'
-import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+import * as UserController from './controllers/UserController.js'
 
 const app = express()
 
@@ -20,76 +18,10 @@ mongoose
 
 app.use(express.json())
 
-app.post('/auth/login', async (req, res) => {
-	try {
-		const user = await UserModel.findOne({ email: req.body.email })
-
-		if (!user) {
-			return res.status(400).json({
-				message: 'Incorrect login or password',
-			})
-		}
-
-		const isValidPass = await bcrypt.compare(
-			req.body.password,
-			user._doc.passwordHash
-		)
-
-		if (!isValidPass) {
-			return res.status(400).json({
-				message: 'Incorrect login or password',
-			})
-		}
-
-		const token = jwt.sign({ _id: user._id }, 'secret123', { expiresIn: '30d' })
-
-		const { passwordHash, ...userData } = user._doc
-
-		res.json({
-			...userData,
-			token,
-		})
-	} catch (err) {
-		console.log(err)
-		res.status(500).json({
-			message: 'Authorisation error',
-		})
-	}
-})
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-	try {
-		const errors = validationResult(req)
-
-		if (!errors.isEmpty()) {
-			return res.status(400).json(errors.array())
-		}
-
-		const password = req.body.password
-		const salt = await bcrypt.genSalt(10)
-		const hash = await bcrypt.hash(password, salt)
-
-		const doc = new UserModel({
-			email: req.body.email,
-			fullName: req.body.fullName,
-			avatarUrl: req.body.avatarUrl,
-			passwordHash: hash,
-		})
-
-		const user = await doc.save()
-
-		const token = jwt.sign({ _id: user._id }, 'secret123', { expiresIn: '30d' })
-
-		const { passwordHash, ...userData } = user._doc
-
-		res.json({ ...userData, token })
-	} catch (err) {
-		console.log(err)
-		res.status(500).json({
-			message: 'Registration error',
-		})
-	}
-})
+// ROUTES
+app.post('/auth/login', UserController.login)
+app.post('/auth/register', registerValidation, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
 const port = 4444
 
